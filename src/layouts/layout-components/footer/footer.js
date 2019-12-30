f26dbdc33b30bb3020582bcaab097dd0d478b4bb
João Paulo Lira <joaopauloliira@gmail.com>
2019-12-30 11:58:52 -0300

    Commit inicial

---
+++ src/layouts/layout-components/footer/footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer className="footer text-center">
+        <a href="https://obra.art.br" target="_blank">
+          &nbsp;
+        </a>
+      </footer>
+    )
+  }
+}
+export default Footer
